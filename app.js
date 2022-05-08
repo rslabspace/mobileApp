aee8efafd088fe4e651b7a72c6a1d4cdfbd61ea9
Chief Muni <muni@rslabspace.com>
2022-05-08 08:49:22 +0000

    new file updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 <html>
 <body>
   <h2>Javascript Main Page</h2>
-  <p id="main">Javascript changes</p>
+  <p id="main">Javascript changes in Gh</p>
   <button type="button" onClick='document.getElementById("main"),innerHTML = "Hey user, Welcome!"'> Submit </button>
 </body>
 </html>
